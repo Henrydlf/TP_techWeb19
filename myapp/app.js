240b534b279474daa76067f4a7d2e81debe5dc09
Henrydlf <36404566+Henrydlf@users.noreply.github.com>
2019-11-18 18:04:06 +0100

    Premier Commit

---
+++ myapp/app.js
@@ -0,0 +1,25 @@
+// import module
+const http = require('http');
+const url = require('url')
+const qs = require('querystring')
+
+const serverHandle = function (req, res) {
+    // Retrieve and print the queryParams
+    const route = url.parse(req.url)
+    const path = route.pathname 
+    const params = qs.parse(url.parse(req.url).query);
+    params['name']='henry';
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    if (path === '/hello' && 'name' in params) {
+        res.write('Hello ' + params['name'])
+    } else {
+        res.write('Hello anonymous')
+    }
+    console.log(params);
+    res.end();
+}
+
+
+const server = http.createServer(serverHandle);
+server.listen(3000); // start he server
