3fcb7d950358e5867388629c5cf8d7578f24a054
Henrydlf <36404566+Henrydlf@users.noreply.github.com>
2019-12-05 22:44:02 +0100

    typescript

--- 02_ExpressJS/app.js
+++ 02_ExpressJS/app.js
@@ -1,7 +1,11 @@
 express = require('express')
 app = express()
+path=require('path')
 
 app.set('port', 3000)
+app.set('views', __dirname + "/views")
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.send('hey please put a name on params')
@@ -11,9 +15,6 @@ app.get('/:name',
     (req, res) => res.render('hello.ejs', {name: req.params.name})
 )
 
-app.set('views', __dirname + "/view")
-app.set('view engine', 'ejs');
-  
 app.listen(
     app.get('port'), 
     () => console.log(`server listening on ${app.get('port')}`)
